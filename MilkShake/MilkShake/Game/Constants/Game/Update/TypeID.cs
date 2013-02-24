abb69a3ea2588587fcd99a0012318e2af9403ce2
Lucas Jones <lucas.jones@dubitlimited.com>
2013-02-24 13:19:39 +0000

    DSFS

---
+++ MilkShake/MilkShake/Game/Constants/Game/Update/TypeID.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Milkshake.Game.Constants.Game.Update
+{
+    public enum TypeID : byte
+    {
+        TYPEID_OBJECT = 0,
+        TYPEID_ITEM = 1,
+        TYPEID_CONTAINER = 2,
+        TYPEID_UNIT = 3,
+        TYPEID_PLAYER = 4,
+        TYPEID_GAMEOBJECT = 5,
+        TYPEID_DYNAMICOBJECT = 6,
+        TYPEID_CORPSE = 7
+    }
+}
