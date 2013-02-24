f77670f4f3d5d0f3b530811d2d9675fae457b6c3
Andrew <andrew.munro@dubitlimited.com>
2013-02-24 12:16:32 +0000

    Whispers yo!

---
+++ MilkShake/MilkShake/Communication/Incoming/World/PCNameQuery.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Milkshake.Network;
+
+namespace Milkshake.Communication.Incoming.World
+{
+    public class PCNameQuery : PacketReader
+    {
+        public UInt32 GUID { get; private set; }
+
+        public PCNameQuery(byte[] data): base(data)
+        {
+            GUID = ReadUInt32();
+        }
+    }
+}
---
+++ MilkShake/MilkShake/Communication/Outgoing/World/PSNameQuery.cs
@@ -0,0 +1,19 @@
+﻿using System.Text;
+using Milkshake.Network;
+using Milkshake.Tools.Database;
+
+namespace Milkshake.Communication.Outgoing.World
+{
+    public class PSNameQuery : ServerPacket
+    {
+        public PSNameQuery(Character character) : base(Opcodes.SMSG_NAME_QUERY_RESPONSE)
+        {
+            Write((uint)character.GUID);
+            Write(Encoding.UTF8.GetBytes(character.Name + '\0'));
+            Write(0);
+            Write((uint)character.Race);
+            Write((uint)character.Gender);
+            Write((uint)character.Class);
+        } 
+    }
+}
