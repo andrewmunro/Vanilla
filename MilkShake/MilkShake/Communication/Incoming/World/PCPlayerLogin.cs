6c08ed53f4c4202189363cdaf6ecf365be3059af
Lucas Jones <lucas.jones@dubitlimited.com>
2013-02-24 01:43:21 +0000

    Added character

---
+++ MilkShake/MilkShake/Communication/Incoming/World/PCPlayerLogin.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Milkshake.Network;
+
+namespace Milkshake.Communication.Incoming.World
+{
+    public class PCPlayerLogin : PacketReader
+    {
+        public uint GUID { get; private set; }
+
+        public PCPlayerLogin(byte[] data) : base(data)
+        {
+            GUID = ReadUInt32();
+        }
+    }
+}
