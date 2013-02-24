6fd48afc119e09a3fe244266d0825e7716be6656
Lucas Jones <lucas.jones@dubitlimited.com>
2013-02-24 00:08:50 +0000

    Chat Out

--- MilkShake/MilkShake/Communication/Outgoing/World/Chat/PSMessageChat.cs
+++ MilkShake/MilkShake/Communication/Outgoing/World/Chat/PSMessageChat.cs
@@ -3,14 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.IO;
+using Milkshake.Game.Constants.Game;
 
 namespace Milkshake.Communication.Outgoing.World.Chat
 {
     internal class PSMessageChat : BinaryWriter
     {
-        public PSMessageChat( soundID) : base(new MemoryStream())
+        public PSMessageChat(ChatMessage type, ChatLanguage language, uint fromGUID, uint toGUID, string message) : base(new MemoryStream())
         {
-            Write(soundID);
+            Write((byte)type);
+            Write((uint)language);
+            Write(fromGUID);
+            Write(toGUID);
+            Write((uint)message.Length);
+            Write(Encoding.UTF8.GetBytes(message + '\0'));
         }
 
         public byte[] Packet
