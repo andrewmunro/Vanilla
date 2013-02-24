40897d833c59f80e22f225960e4ab894cbe2d3f6
Lucas Jones <lucas.jones@dubitlimited.com>
2013-02-24 13:11:38 +0000

    Bamm

---
+++ MilkShake/MilkShake/Communication/Outgoing/World/Logout2/SCLogoutResponse.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Milkshake.Network;
+
+namespace Milkshake.Communication.Outgoing.World.Logout
+{
+    class SCLogoutResponse : BinaryWriter
+    {
+        public SCLogoutResponse() : base(new MemoryStream())
+        {
+            Write((UInt32) 0);
+            Write((byte) 0);
+        }
+    }
+}
