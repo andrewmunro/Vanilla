2127a07ca19595eb0c9e38c6bccb0e1bb794447b
Lucas Jones <Necxii@gmail.com>
2013-06-30 15:58:05 +0100

    Added InventorySlotID enum

---
+++ MilkShake/MilkShake/Game/Constants/Game/World/Item/InventorySlotID.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Milkshake.Game.Constants.Game.World.Item
+{
+    public enum InventorySlotID : byte
+    {
+        None,
+        Head,
+        Neck,
+        Shoulders,
+        Shirt,
+        Vest,
+        Waist,
+        Legs,
+        Feet,
+        Wrist,
+        Hands,
+        Ring,
+        Trinket,
+        Onehand,
+        Shield,
+        Bow,
+        Back,
+        Twohand,
+        Bag,
+        Tabbard,
+        Robe,
+        Mainhand,
+        Offhand,
+        Held,
+        Ammo,
+        Thrown,
+        Ranged,
+        Ranged2,
+        Relic
+    }
+}
