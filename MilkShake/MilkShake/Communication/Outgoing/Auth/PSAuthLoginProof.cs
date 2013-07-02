41b1070795acf6599c2adc64986e59b867d57f2a
Andrew Munro <andrew.munro@dubitlimited.com>
2013-07-02 12:45:03 +0100

    refactoring session

---
+++ MilkShake/MilkShake/Communication/Outgoing/Auth/PSAuthLoginProof.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Milkshake.Game.Constants;
+using Milkshake.Network;
+using Milkshake.Tools.Cryptography;
+using Milkshake.Tools.Extensions;
+
+namespace Milkshake.Communication.Outgoing.Auth
+{
+    class PSAuthLoginProof : ServerPacket
+    {
+        public PSAuthLoginProof(SRP6 Srp6) : base(AuthOpcodes.AUTH_LOGIN_PROOF)
+        {
+            Write((byte)1);
+            Write((byte)AccountStatus.Ok);
+            Write(Srp6.M2);
+            this.WriteNull(4);
+        }
+    }
+}
---
+++ MilkShake/MilkShake/Communication/Outgoing/Auth/PSRealmList.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Milkshake.Game.Constants.Login;
+using Milkshake.Network;
+using Milkshake.Tools.Config;
+using Milkshake.Tools.Extensions;
+
+namespace Milkshake.Communication.Outgoing.Auth
+{
+    class PSRealmList : ServerPacket
+    {
+        public PSRealmList() : base(AuthOpcodes.REALM_LIST)
+        {
+            Write((uint)0);
+            Write((byte)1);
+            Write((uint)RealmType.PVP);
+            Write((uint)0);
+            this.WriteCString(INI.GetValue(ConfigValues.WORLD, ConfigValues.NAME));
+            this.WriteCString(INI.GetValue(ConfigValues.WORLD, ConfigValues.IP) + ":" + INI.GetValue(ConfigValues.WORLD, ConfigValues.PORT));
+            Write((float)INI.GetValue<float>(ConfigValues.WORLD, ConfigValues.POPULATION)); // Pop
+            Write((byte)3); // Chars
+            Write((byte)1); // time
+            Write((byte)0); // time
+            Write((UInt16)0x0002); 
+        }
+    }
+}
