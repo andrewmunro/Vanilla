09bb6d1c8831ebd1523a5a0f834e6acd1284f242
Lucas Jones <Necxii@gmail.com>
2013-10-09 23:51:13 +0100

    Added Quick Cache Hack for DB Updated Vanilla.Login.csproj

--- Vanilla/Vanilla.Login/VanillaLogin.cs
+++ Vanilla/Vanilla.Login/VanillaLogin.cs
@@ -1,6 +1,9 @@
-﻿using Vanilla.Core.Components;
+﻿using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using Vanilla.Core.Components;
+using Vanilla.Core.Logging;
 using Vanilla.Login.Components;
-using Vanilla.Login.Components.Login;
+using Vanilla.Login.Components.Auth;
 using Vanilla.Login.Components.Realm;
 using Vanilla.Login.Database.Models;
 using Vanilla.Login.Network;
@@ -15,6 +18,8 @@ namespace Vanilla.Login
         public VanillaLogin(int port, int maxConnection) : base()
         {
             LoginDB = new LoginDatabase();
+            LoginDB.Accounts.ToList(); // Cache Hack
+
             Server = new LoginServer();
 
             Components.Add(new AuthComponent(this));
