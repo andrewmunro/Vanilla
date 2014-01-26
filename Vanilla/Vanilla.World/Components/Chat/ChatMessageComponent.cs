61d7e6cbb4c84602d914879ded55ce2c64db2905
Andrew Munro <andrew.munro@dubitlimited.com>
2014-01-26 22:31:25 +0000

    Hotfix.

--- Vanilla/Vanilla.World/Components/Chat/ChatMessageComponent.cs
+++ Vanilla/Vanilla.World/Components/Chat/ChatMessageComponent.cs
@@ -76,13 +76,8 @@
         public void OnSayYell(WorldSession session, PCMessageChat packet)
         {
             if (packet.Message[0].ToString() == Config.GetValue(ConfigSections.WORLD, ConfigValues.COMMAND_KEY)) ChatCommandParser.ExecuteCommand(session, packet.Message);
-<<<<<<< HEAD
-            else Server.TransmitToAll(new PSMessageChat(packet.Type, ChatMessageLanguage.LANG_UNIVERSAL, (ulong)session.Player.ObjectGUID.RawGUID, packet.Message));
-=======
             else Server.TransmitToAll(new PSMessageChat(packet.Type, ChatMessageLanguage.LANG_UNIVERSAL, session.Player.ObjectGUID.RawGUID, packet.Message));
-
             session.sendMessage(packet.Message);
->>>>>>> Added missing packet handler for Query (Enables Chat)
         }
 
         public void OnWhisper(WorldSession session, PCMessageChat packet)
@@ -91,8 +86,8 @@
 
             if (remoteSession != null)
             {
-                session.SendPacket(new PSMessageChat(ChatMessageType.CHAT_MSG_WHISPER_INFORM, ChatMessageLanguage.LANG_UNIVERSAL, remoteSession.Player.ObjectGUID.Low, packet.Message)); 
-                remoteSession.SendPacket(new PSMessageChat(ChatMessageType.CHAT_MSG_WHISPER, ChatMessageLanguage.LANG_UNIVERSAL, session.Player.ObjectGUID.Low, packet.Message)); 
+                session.SendPacket(new PSMessageChat(ChatMessageType.CHAT_MSG_WHISPER_INFORM, ChatMessageLanguage.LANG_UNIVERSAL, remoteSession.Player.ObjectGUID.RawGUID, packet.Message)); 
+                remoteSession.SendPacket(new PSMessageChat(ChatMessageType.CHAT_MSG_WHISPER, ChatMessageLanguage.LANG_UNIVERSAL, session.Player.ObjectGUID.RawGUID, packet.Message)); 
             }
             else
             {
@@ -112,7 +107,7 @@
 
             if (channel.Sessions.Count == 0) ChatChannels.Remove(channel);
 
-            session.SendPacket(new PSChannelNotify(ChatChannelNotify.CHAT_YOU_LEFT_NOTICE, session.Player.ObjectGUID.Low, packet.ChannelName));
+            session.SendPacket(new PSChannelNotify(ChatChannelNotify.CHAT_YOU_LEFT_NOTICE, session.Player.ObjectGUID.RawGUID, packet.ChannelName));
         }
 
         private void OnJoinChannel(WorldSession session, PCChannel packet)
@@ -125,13 +120,13 @@
             }
             channel.Sessions.Add(session);
 
-            session.SendPacket(new PSChannelNotify(ChatChannelNotify.CHAT_YOU_JOINED_NOTICE, session.Player.ObjectGUID.Low, packet.ChannelName));
+            session.SendPacket(new PSChannelNotify(ChatChannelNotify.CHAT_YOU_JOINED_NOTICE, session.Player.ObjectGUID.RawGUID, packet.ChannelName));
         }
 
         private void OnChannelMessage(WorldSession session, PCMessageChat packet)
         {
             var channel = ChatChannels.SingleOrDefault(c => c.Name == packet.ChannelName);
-            channel.Sessions.ForEach(s => s.SendPacket(new PSMessageChat(ChatMessageType.CHAT_MSG_CHANNEL, ChatMessageLanguage.LANG_UNIVERSAL, session.Player.ObjectGUID.Low, packet.Message, packet.ChannelName)));
+            channel.Sessions.ForEach(s => s.SendPacket(new PSMessageChat(ChatMessageType.CHAT_MSG_CHANNEL, ChatMessageLanguage.LANG_UNIVERSAL, session.Player.ObjectGUID.RawGUID, packet.Message, packet.ChannelName)));
         }
 
         public void SendSytemMessage(WorldSession session, string message)
