63fa188ae47d1468c559800b3593353761ab575f
Lucas Jones <Necxii@gmail.com>
2014-01-26 22:29:11 +0000

    Missing File

---
+++ Vanilla/Vanilla.World/Components/Chat/Packets/Outgoing/PSNameQueryResponse.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanilla.Core.Network.Packet;
+using Vanilla.Core.Opcodes;
+
+namespace Vanilla.World.Components.Chat.Packets.Outgoing
+{
+    public class PSNameQueryResponse : WorldPacket
+    {
+        private string channel;
+
+        public PSNameQueryResponse(Vanilla.Database.Character.Models.Character character)
+            : base(WorldOpcodes.SMSG_NAME_QUERY_RESPONSE)
+        {
+            Write((ulong)character.GUID);
+            Write(Encoding.UTF8.GetBytes(character.Name + '\0'));
+            Write((byte)0); // realm name for cross realm BG usage
+            Write((uint)character.Race);
+            Write((uint)character.Gender);
+            Write((uint)character.Class);
+        }
+    }
+}
