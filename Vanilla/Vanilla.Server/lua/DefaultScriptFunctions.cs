5bad63c664793de37c3b4a6b59808a3ab8c0f989
Lucas Jones <lucas.jones@dubitlimited.com>
2014-05-13 13:47:04 +0100

    Started JS Script Engine

---
+++ Vanilla/Vanilla.Server/lua/DefaultScriptFunctions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vanilla.Server.lua
+{
+    public class Debug
+    {
+        public void trace(object _object)
+        {
+            if (_object == null) _object = "Null";
+            Console.WriteLine("[LUA] " + _object.ToString());
+        }
+    }
+}
---
+++ Vanilla/Vanilla.Server/lua/LuaFunction.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vanilla.Server.lua
+{
+    [System.AttributeUsage(System.AttributeTargets.Method)]
+    public class LUAFunction : System.Attribute
+    {
+        public LUAFunction()
+        {
+        }
+    }
+}
